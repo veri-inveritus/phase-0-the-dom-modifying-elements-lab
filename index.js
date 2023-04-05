727a6eff47c10b9fe6ad644cb5eb9a9cc4fbdab8
David <yidavidh@outlook.com>
2023-04-04 22:06:40 -0700

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+let main = document.querySelector('main');
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "David is the champion";
